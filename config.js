bee15cf018bbe31c2ec398e581a9ada4bca9dd5a
henrypoels-ux <henry.poels@gmail.com>
2026-02-07 18:27:59 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@
  * - TRIP_SLUG: deine Trip-ID (z.B. frankreich-2026)
  */
 window.APP_CONFIG = {
-  SUPABASE_URL: "PASTE_SUPABASE_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_SB_PUBLISHABLE_KEY_HERE",
+  SUPABASE_URL: "https://gcgkeqakpwiufnefkibs.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_xPhjJnigAOtNu4KFAcZ0Yw_hLaopSQU",
   TRIP_SLUG: "frankreich-2026",
   AUTOLOAD_PUBLIC: true
 };
